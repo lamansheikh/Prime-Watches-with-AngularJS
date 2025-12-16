a24a1b0812209cfa5c875e2da4822aa2aeb3c04d
lamansheikh <nomisheikh382@gmail.com>
2025-12-16 14:47:55 +0500

    Footer Update

--- src/app/components/pages/home/home.ts
+++ src/app/components/pages/home/home.ts
@@ -3,10 +3,12 @@ import { Navbar } from '../../navbar/navbar';
 import { Banner } from './banner/banner';
 import { Cards } from './cards/cards';
 import { SubscriptionBanner } from './subscription-banner/subscription-banner';
+import { Testimonials } from './testimonials/testimonials';
+import { Footer } from '../../footer/footer';
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar ,Banner, Cards, SubscriptionBanner],
+  imports: [Navbar ,Banner, Cards, SubscriptionBanner, Testimonials, Footer],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
